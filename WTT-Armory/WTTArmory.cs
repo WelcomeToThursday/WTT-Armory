b1533458ad3d3c666e9cbd9a67e6aec3f95debb7
Tron111111122222 <eagentile@sbcglobal.net>
2026-08-30 12:46:34 -0500

    more version fixes

--- WTT-Armory/WTTArmory.cs
+++ WTT-Armory/WTTArmory.cs
@@ -17,7 +17,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Author { get; init; } = "GrooveypenguinX";
     public override List<string>? Contributors { get; init; } = null;
     public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
-    public override Range SptVersion { get; init; } = new("~4.0.2");
+    public override Range SptVersion { get; init; } = new("~4.0.13");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
     {
